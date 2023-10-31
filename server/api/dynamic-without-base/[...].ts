592be5fc3e53fda29647f1a746c667497c172b36
Tom Aratyn <taratyn@tucows.com>
2023-10-31 16:19:52 -0400

    Updated dynamic routes to be configured based on an object.

--- server/api/dynamic-without-base/[...].ts
+++ server/api/dynamic-without-base/[...].ts
@@ -1,8 +1,20 @@
 import {createRouter,  useBase, defineEventHandler, H3Event} from "h3";
 
 const router = createRouter()
+const prefix = '/api/dynamic-without-base';
 
-router.get('/api/dynamic-without-base/test', defineEventHandler((e: H3Event) => {
+const config = {
+    'foobar': 'foobar',
+    'foo/ooo/bar': 'foo/ooo/bar',
+}
+
+Object.entries(config).forEach(([key, value]) => {
+    router.get(prefix + '/' + key, defineEventHandler((e: H3Event) => {
+        return value
+    }))
+})
+
+router.get(prefix + '/test', defineEventHandler((e: H3Event) => {
     return 'Hello World'
 }))
 
